e9578716d7a0f62cd2ccb630b3e0e3b44d5f4914
omegascorp <omegascorp@gmail.com>
2016-04-21 00:36:16 +0400

    Add lcov report.

--- karma.conf.js
+++ karma.conf.js
@@ -15,12 +15,12 @@ module.exports = function(config) {
             'test/*'
         ],
         browsers: ['PhantomJS'],
-        reporters: ['mocha', 'coverage'],
+        reporters: ['mocha', 'coverage', 'coveralls'],
         preprocessors: {
-            'src/**/*.js': ['coverage']
+            'src/**/*.js': ['coverage', 'coveralls']
         },
         coverageReporter: {
-            type : 'html',
+            type : 'lcov',
             dir : 'coverage/'
         }
     });
